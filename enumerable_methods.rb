52bbbdb2ea1b5027be42cf5ecb8c055a93d54d93
thsvr <vr.ths.zd@gmail.com>
2019-09-27 19:03:19 -0300

    add my_each method

---
+++ enumerable_methods.rb
@@ -0,0 +1,61 @@
+module Enumerable
+
+#my_each
+def my_each
+    i = 0
+    while i < self.size
+        yield(self[i])  
+        i+=1      
+    end
+    self
+  end
+end
+
+#my_each_with_index
+def each_with_index
+
+end
+
+#my_select 
+def my_select
+
+end
+
+#my_all?
+def my_all?
+
+end
+
+#my_any?
+def my_any?
+
+end
+
+#my_none?
+def my_none?
+
+end
+
+#my_count
+def my_count
+
+end
+
+#my_map
+def my_map
+
+end
+
+#my_inject
+def my_inject
+
+end
+
+
+
+
+
+
+end #end of module enumerable
+
+
