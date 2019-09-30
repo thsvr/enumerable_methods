c25015bf355abbe54d25fe6ebc4ad990f174e294
thsvr <vr.ths.zd@gmail.com>
2019-09-30 18:42:59 -0300

    add method my_select

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -12,14 +12,18 @@ module Enumerable
 
     #my_each_with_index 2
     def my_each_with_index
-        for x in 0...self.length
+        for x in 0...self.size
             yield self[x], x
         end
     end
 
-    #my_select 
+    #my_select 3
     def my_select
-
+        selected_elements = []
+        self.my_each do |x|
+            selected_elements << x if yield x
+        end
+        selected_elements
     end
 
     #my_all?
@@ -67,4 +71,10 @@ array.my_each { |number| puts number ** 3}
 array.each_with_index {|number, index| puts array [number] = index}
 puts ""
 array.my_each_with_index {|number, index| puts array [number] = index}
+=end
+
+=begin my_select 3 working
+array.select {|num| puts num.even? }
+puts ""
+array.my_select {|num| puts num.even? }
 =end
