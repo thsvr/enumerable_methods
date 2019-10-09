df3769e6e103df86b1a7afbe51ab11fbce868500
thsvr <vr.ths.zd@gmail.com>
2019-10-09 18:09:15 -0300

    add my_each test

--- spec/enumerable_methods_spec.rb
+++ spec/enumerable_methods_spec.rb
@@ -5,4 +5,17 @@ require_relative '../enumerable_methods.rb'
 # good
 RSpec.describe Enumerable do
     let(:array) {[1, 2, 3, 4, 5, 6, 7]}
-end 
+
+    describe "#my_each" do
+    it "returns the array itself" do
+        
+        default_arr = []
+        tested_arr = []
+
+        array.each {|x| default_arr << x + 1}
+        array.my_each {|x| tested_arr << x + 1}        
+        expect(tested_arr).to eql(default_arr)
+    end
+end
+
+end #end of the method?
